579ca913f96e8737342ee00cf795ea5871f3eb93
Yang <sdada0112@gmail.com>
2022-05-31 21:19:39 +0900

    220531

--- library.py
+++ library.py
@@ -3,38 +3,41 @@ import random
 import os
 import time
 import sqlite3
+import datetime
+from datetime import date
+
 
 conn = sqlite3.connect ('book.db')
 cur = conn.cursor()
 
-save_book = []
-book_cnt = 0  
+# save_book = []
+# book_cnt = 0  
 result=[] #검색결과 저장
 basket=[] #장바구니에 저장
-user_info =[]
-plus=0
+# user_info =[]
+# plus=0
 userID =''
-donation_list = [['앨리스 죽이기', '코바야시 야스미'], ['거울나라의 앨리스', '루이스 캐럴'], ['앨리스 지금이야', '김종원']]
-#배열 마지막, 1:대여가능 2:대여중
-BookList=[[1, '작별인사', '김영하', 1],
-          [2, '이기적 유전자', '리처드 도킨스', 1],
-          [3, '게놈 익스프레스', '조진호', 1],
-          [4, '데미안', '헤르만 헤세', 1],
-          [5, '언어의 온도', '이기주', 1],
-          [6, '나미야 잡화점의 기적', '히가시노 게이고', 1],
-          [7, '인간 실격', '다자이 오사무', 1]]
+# donation_list = [['앨리스 죽이기', '코바야시 야스미'], ['거울나라의 앨리스', '루이스 캐럴'], ['앨리스 지금이야', '김종원']]
+# #배열 마지막, 1:대여가능 2:대여중
+# BookList=[[1, '작별인사', '김영하', 1],
+#           [2, '이기적 유전자', '리처드 도킨스', 1],
+#           [3, '게놈 익스프레스', '조진호', 1],
+#           [4, '데미안', '헤르만 헤세', 1],
+#           [5, '언어의 온도', '이기주', 1],
+#           [6, '나미야 잡화점의 기적', '히가시노 게이고', 1],
+#           [7, '인간 실격', '다자이 오사무', 1]]
 
-class information:
-    global user_info
-    def user_plus(self, ID, PW, name, phone):        
-        global plus
-        user_info.append([])
-        user_info[plus].append(ID)
-        user_info[plus].append(PW)
-        user_info[plus].append(name)
-        user_info[plus].append(phone)
-        plus += 1
-        print(user_info)
+# class information:
+#     global user_info
+#     def user_plus(self, ID, PW, name, phone):        
+#         global plus
+#         user_info.append([])
+#         user_info[plus].append(ID)
+#         user_info[plus].append(PW)
+#         user_info[plus].append(name)
+#         user_info[plus].append(phone)
+#         plus += 1
+#         print(user_info)
 
 
 def login():
@@ -55,7 +58,7 @@ def login():
             lose = int(input("전화번호를 입력해주세요. > "))
             for search in user_info:
                 if lose == search[3]:
-                    print("{0}님의 ID와 PW는 {1}, {2}입니다".format(search[0], search[1], search[2]))
+                    print("{0}님의 ID와 PW는 {1}, {2}입니다".format(search[0], search[2], search[3]))
                     return
                 else:
                     print("일치하는 회원정보가 없습니다.\n")
@@ -68,7 +71,7 @@ def login():
             lose = int(input("전화번호를 입력해주세요. > "))
             for search in user_info:
                 if lose ==search[3]:
-                    print("{0}님의 ID와 PW는 {1}, {2}입니다".format(search[0], search[1], search[2]))
+                    print("{0}님의 ID와 PW는 {1}, {2}입니다".format(search[0], search[2], search[3]))
                     return
                 else:
                     print("일치하는 회원정보가 없습니다.\n")
@@ -76,7 +79,7 @@ def login():
       
         for search in user_info:
             print(search)
-            if ID == search[1] and PW == search[2]:#아이디,패스워드 일치하면
+            if ID == search[2] and PW == search[3]:#아이디,패스워드 일치하면
                 userID=ID
                 print("로그인 되었습니다.")
                 time.sleep(0.5)
@@ -91,7 +94,7 @@ def login():
         phone = input("전화번호를 입력해주세요. > ")
         ID = input("ID를 입력해주세요 > ")
         PW = input("PW를 입력해주세요 > ")
-        cur.execute('insert into userInfo values (?, ?, ?, ?)', (name, ID, PW, phone))
+        cur.execute('insert into userInfo values (?, ?, ?, ?, ?, ? ,?)', (name, phone, ID, PW, None, None, None))
         conn.commit()
 
     else:
@@ -128,21 +131,24 @@ def rental(): #대여 함수
     global book_cnt
     k=1                      
     print(basket)
-    print(userID)
     user_input = str(input('대여하시겠습니까? y/n  > '))
     cur.execute("SELECT Num, Title, Author FROM book")
+    rentDate=date.today()
+    rentDate=rentDate.isoformat() #문자열로 변환시켜줌
+    
     row=cur.fetchall()
     if user_input == 'y' or user_input == 'Y':
         for i in row:
             for j in basket:
                 if i[0] == j[0]:
                     data='book'+(str(k))
-                    ID_BookName = str(j[0])+'/'+j[1]
+                    ID_BookName_Date = str(j[0])+'/'+j[1]+'/'+rentDate
                     print(data)
-                    print(ID_BookName)
+                    print(userID)
+                    print(ID_BookName_Date)
                     cur.execute("update book set Rental = '0' where Num=?", (j[0],))
                     query = "update userInfo set %s = ? where userID = ?" %data
-                    cur.execute(query,(ID_BookName, userID))
+                    cur.execute(query,(ID_BookName_Date, userID))
                     conn.commit()
                     k+=1
         del basket[:]              
@@ -224,65 +230,81 @@ def menu():                                             #조회 함수
             print("잘못 입력하셨습나다.")
 
 def book_return():  #반납
-    cur.execute("SELECT * FROM book")
-    turn_book = cur.fetchall()
-
+    global userID
+    print(userID)
     return_choice = input("1. 책제목  2. 도서 고유번호  > ")
     if return_choice == '1':
         re_book = input("반납할 책의 제목을 입력해주세요. > ")
-        for search in turn_book:           
-            if re_book == search[1]:
-                print(search)
-                if search[3] == 0: #대여중인 책일때   
-                    print("반납되었습니다.")
-                    cur.execute("update book set Reantal = ? where Author = ?",(1, re_book))
-                else:
-                    print("대여중인 책이 아닙니다.")
-                    break 
+        cur.execute("SELECT book1, book2, book3 FROM userInfo where userID= ?", (userID, ))
+        turn_book = cur.fetchone()
+        turn_book = list(turn_book)
+        for i in range(1, 4):
+            divide = turn_book[i-1].split('/')           
+            if turn_book[i-1] == None:
+                break
+            if re_book in turn_book[i-1]:  
+                print("반납되었습니다.") 
+                print(divide)
+                cur.execute("update book set Rental = ? where Num = ?",(1, divide[0]))
+                conn.commit() 
+                query = "update userInfo set book%d = NULL where userID = ?" % i
+                cur.execute(query,(userID, ))
+                conn.commit()
+                break 
+
             else:
-                continue   
+                print("대여중인 책이 아닙니다.")
+                break  
 
     if return_choice == '2':
-        re_book_num = int(input("반납할 책의 고유번호를 입력해주세요. > "))
-        for search in turn_book:
-            if search[0] == re_book_num:
-                if search[3] == 0:
-                    print("반납되었습니다.")
-                    cur.execute("update book set Rental = ? where Num = ?",(1, re_book_num)) 
-                    conn.commit()
-                    break
-                else:
-                    print("대여중인 책이 아닙니다.")
-
+        re_book_num = input("반납할 책의 고유번호를 입력해주세요. > ")
+        cur.execute("SELECT book1, book2, book3 FROM userInfo where userID= ?", (userID, ))
+        turn_book = cur.fetchone()
+        print(turn_book)
+        turn_book = list(turn_book)
+        for i in range(1, 4):
+            if turn_book[i-1] == None :
+                break
+            if re_book_num in turn_book[i-1]:
+                print("반납되었습니다.")
+                re_book_num = int(re_book_num)
+                cur.execute("update book set Rental = ? where Num = ?",(1, re_book_num))
+                conn.commit()
+                query = "update userInfo set book%d = NULL where userID = ?" % i
+                cur.execute(query,(userID, ))
+                conn.commit()
+                break
+            else:
+                print("대여중인 책이 아닙니다.")
 
 def mine_info():
     global save_book
     print("=======나의정보=======")
     print("대여중인 도서 리스트")
-    for i in range(len(BookList)):
-        if BookList[i][3] == 0:
-            print("-{0}".format(BookList[i][1]))
-    print("")
+    # for i in range(len(BookList)):
+    #     if BookList[i][3] == 0:
+    #         print("-{0}".format(BookList[i][1]))
+    # print("")
     print("대여 후 반납한 도서 리스트")
     print("-{0}\n".format(save_book))
     print("연체 조회 정보")
     print("연체된 책이 없습니다.")
     print("")
     user_input = input("정보 변경을 원하시나요? (y/n) >")
-    if user_input == 'y':
-        ID = input("ID를 입력해주세요. > ")
-        for i in range(len(user_info)):
-            if ID == user_info[i][0]:
-                change = input("비밀번호를 변경하시겠습니까. (y/n) > ")
-                if change == 'y':
-                    change_PW = input("변결할 PW > ")
-                    user_info[i][1] = change_PW
-                    print(user_info)
-                elif change == 'n':
-                    pass
+    # if user_input == 'y':
+    #     ID = input("ID를 입력해주세요. > ")
+    #     for i in range(len(user_info)):
+    #         if ID == user_info[i][0]:
+    #             change = input("비밀번호를 변경하시겠습니까. (y/n) > ")
+    #             if change == 'y':
+    #                 change_PW = input("변결할 PW > ")
+    #                 user_info[i][1] = change_PW
+    #                 print(user_info)
+    #             elif change == 'n':
+    #                 pass
 
-    elif user_input == 'n':
-        pass
+    # elif user_input == 'n':
+    #     pass
     print("======================")
 
 def main_screen():
@@ -291,8 +313,8 @@ def main_screen():
 
     print(" ")
     print("------추천도서------ ")
-    print("{0:^14}".format(BookList[i][1]))
-    print("{0:^14}".format(BookList[j][1]))
+    # print("{0:^14}".format(BookList[i][1]))
+    # print("{0:^14}".format(BookList[j][1]))
     print("--------------------")
     print(" ")
 
